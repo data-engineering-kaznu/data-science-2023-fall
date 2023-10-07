9b360289793e3378128772dc588fefead7372b52
amangedlie <amangeldiersultan248@gmail.com>
2023-10-07 12:38:03 +0600

    [hw2][xs]:added hw2

---
+++ amangeldiyersultan/hw2_amangeldie/task2_py.py
@@ -0,0 +1,42 @@
+import csv
+from datetime import datetime
+
+with open('task2_1.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    field = ["year", "region", "value"]
+
+    writer.writerow(field)
+    writer.writerow(["2022-Feb", "Almaty", "130050"])
+    writer.writerow(["2022-Feb", "Astana", "130050"])
+    writer.writerow(["2022-Mar", "Almaty", "130050"])
+    writer.writerow(["2022-Mar", "Astana", "130050"])
+
+input_file_path = 'task2_1.csv'
+output_file_path = 'task2_2.csv'
+
+date_column_index = 0
+
+current_date_format = '%Y-%b'
+
+new_date_format = '%Y-%m-%d'
+
+with open(input_file_path, 'r', newline='') as input_file, \
+        open(output_file_path, 'w', newline='') as output_file:
+    csvreader = csv.reader(input_file)
+    csvwriter = csv.writer(output_file)
+
+    for row in csvreader:
+        try:
+            old_date_str = row[0]
+            new_date_str = datetime.strptime(old_date_str, current_date_format).strftime(new_date_format)
+            row[0] = new_date_str
+            csvwriter.writerow(row)
+        except ValueError:
+            csvwriter.writerow(row)
+
+
+with open('task2_2.csv', 'r', newline='') as csvfile:
+    csvreader = csv.reader(csvfile)
+
+    for row in csvreader:
+        print(row)
